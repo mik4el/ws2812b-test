2d99b00727f6433bb4e71b61f07e3fe43aa8ded8
Mikael Andersson <mik4el.4ndersson@gmail.com>
2017-04-13 13:15:39 +0200

    tested different visualizations

--- ws2812b-test/ws2812b-test.ino
+++ ws2812b-test/ws2812b-test.ino
@@ -43,11 +43,11 @@ void setup() {
 
 void loop() { 
   // Turn the LED on, then pause
-  leds[0] = CRGB::Red;
+  leds[2] = CRGB::Red;
   FastLED.show();
   delay(500);
   // Now turn the LED off, then pause
-  leds[0] = CRGB::Black;
+  leds[2] = CRGB::Black;
   FastLED.show();
   delay(500);
 }
